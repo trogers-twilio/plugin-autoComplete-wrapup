d38538413a4e62f81b598e32848475942f1e1d9a
Cheryl Jose <cherylmj01@gmail.com>
2022-02-25 13:50:00 -0800

    Added the logic for handling autowrapup when the agent refreshes the browser

--- src/AutoCompleteWrapupPlugin.js
+++ src/AutoCompleteWrapupPlugin.js
@@ -3,7 +3,7 @@ import { VERSION } from '@twilio/flex-ui';
 import { FlexPlugin } from 'flex-plugin';
 import ConfigureFlexStrings from './flex-hooks/strings';
 import RegisterFlexNotifications from './flex-hooks/notifications';
-
+import FlexState from './states/FlexState';
 
 
 const PLUGIN_NAME = 'AutoCompleteWrapupPlugin';
@@ -14,12 +14,12 @@ export default class AutoCompleteWrapupPlugin extends FlexPlugin {
   }
 
   /**
-   * This code is run when your plugin is being started
-   * Use this to modify any UI components or attach to the actions framework
-   *
-   * @param flex { typeof import('@twilio/flex-ui') }
-   * @param manager { import('@twilio/flex-ui').Manager }
-   */
+  * This code is run when your plugin is being started
+  * Use this to modify any UI components or attach to the actions framework
+  *
+  * @param flex { typeof import('@twilio/flex-ui') }
+  * @param manager { import('@twilio/flex-ui').Manager }
+  */
   async init(flex, manager) {
 
     ConfigureFlexStrings(flex, manager);
@@ -28,17 +28,40 @@ export default class AutoCompleteWrapupPlugin extends FlexPlugin {
     // get the wrapupConfig config from ui_attributes
     const wrapupConfig = manager.serviceConfiguration?.ui_attributes?.autoWrapupTimer;
 
-    // if the configuration for autoWrapupTimer exits then follow the logic below
+    // if the configuration for autoWrapupTimer exists then follow the logic below
     if (wrapupConfig) {
-      manager.workerClient.on('reservationCreated', reservation => {
-        // get the reservation
-        const trueReservation = reservation.addListener?reservation:reservation.source;
+      //reservation existing
+      FlexState.workerTasks.forEach(reservation => {
 
+        const existing_reservation = reservation;
+        let reservation_channelName = existing_reservation.taskChannelUniqueName;
+        let reservaton_channelWrapUpConfiguration = wrapupConfig[reservation_channelName];
+
+        if(existing_reservation.status === "wrapping"){
+
+          let date_last_updated = existing_reservation.dateUpdated;
+          let age_so_far_ms = (new Date() - new Date(date_last_updated));
+          let reservation_wrapUpTime_seconds = reservaton_channelWrapUpConfiguration.maxSeconds;
+
+          let remaining_time = (reservation_wrapUpTime_seconds*1000) - age_so_far_ms;
+          remaining_time = remaining_time <= 0 ? 1 : remaining_time;
+
+          if (reservaton_channelWrapUpConfiguration && reservaton_channelWrapUpConfiguration.enabled){
+            setTimeout(()=> {
+              flex.Actions.invokeAction('CompleteTask', { sid: reservation.sid });
+            }, remaining_time);
+          }
+
+        }
+      });
+
+      manager.workerClient.on('reservationCreated', reservation => {
         // get the channel name for the reservation
-        const channelName = trueReservation.task.taskChannelUniqueName;
+        let trueReservation = reservation.addListener?reservation:reservation.source;
+        let channelName = trueReservation.task.taskChannelUniqueName;
 
         //get the config time from the ui_attributes for the voice channel
-        const channelWrapUpConfiguration = wrapupConfig[channelName];
+        let channelWrapUpConfiguration = wrapupConfig[channelName];
 
         // listen to the wrap up event for the reservation
         trueReservation.addListener('wrapup', payload => {
@@ -47,18 +70,15 @@ export default class AutoCompleteWrapupPlugin extends FlexPlugin {
           if (channelWrapUpConfiguration && channelWrapUpConfiguration.enabled){
             setTimeout(()=> {
               flex.Actions.invokeAction('CompleteTask', { sid: reservation.sid });
-
-            },channelWrapUpConfiguration.maxSeconds*1000);
+            }, channelWrapUpConfiguration.maxSeconds*1000);
           }
         });
-
       });
-  }
+    }
 
-  // else show a notification that the configuration does not exist
-  else {
+    // else show a notification that the configuration does not exist
+    else {
       flex.Notifications.showNotification('UnconfiguredAutoWrapupTimer');
-  }
-
+    }
   }
 }
