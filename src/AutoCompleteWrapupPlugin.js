0b7160c7ea6cecf6f8d2b13ce2cb178add0e0e5d
Cheryl Jose <cherylmj01@gmail.com>
2022-05-17 09:08:54 -0700

    Updated the code to wrap up tasks when agent is lpggong out

--- src/AutoCompleteWrapupPlugin.js
+++ src/AutoCompleteWrapupPlugin.js
@@ -3,8 +3,8 @@ import { VERSION } from '@twilio/flex-ui';
 import { FlexPlugin } from '@twilio/flex-plugin';
 import ConfigureFlexStrings from './flex-hooks/strings';
 import RegisterFlexNotifications from './flex-hooks/notifications';
-import {checkExistingReservation} from './helper/existingReservation';
-
+// import {checkExistingReservation} from './helper/existingReservation';
+import FlexState from './states/FlexState';
 
 const PLUGIN_NAME = 'AutoCompleteWrapupPlugin';
 
@@ -21,61 +21,65 @@ export default class AutoCompleteWrapupPlugin extends FlexPlugin {
   * @param manager { import('@twilio/flex-ui').Manager }
   */
   async init(flex, manager) {
-
     ConfigureFlexStrings(flex, manager);
     RegisterFlexNotifications(flex, manager);
-   
 
-    // This plugin should only load after other plugins have loaded
     manager.events.addListener('pluginsLoaded', () => {
-
       // get the wrapupConfig config from ui_attributes
       const wrapupConfig = manager.serviceConfiguration?.ui_attributes?.autoWrapupTimer;
 
-      // if the configuration for autoWrapupTimer exists then follow the logic below
       if (wrapupConfig) {
-
         // declare a map to store the timers
         // this map will be passed to clearTimeout in case the agent finishes the task before autoComplete executes
         const timers = new Map();
 
-        let reservationExists = checkExistingReservation();
-        // console.log('testValue',reservationExists);
+        FlexState.workerTasks.forEach(reservation => {
 
-      
-        if (reservationExists.wrapping === true){
-
-          // Get the channel name for existing reservation
-          let reservation_channelName = reservationExists.existing_reservation.taskChannelUniqueName;
-
-          // Get the configuration for the channel
-          let reservaton_channelWrapUpConfiguration = wrapupConfig[reservation_channelName];
-          // console.log('Configuration',reservaton_channelWrapUpConfiguration)
-
+          const existing_reservation = reservation;
+          let reservation_channelName = existing_reservation.taskChannelUniqueName;
+          let reservation_channelWrapUpConfiguration = wrapupConfig[reservation_channelName];
+          
           // set a variable for timeout, this is used for clearing timeouts if the task is completed
-          let existingReservation_timer ;
+          let existingReservation_timer;
 
-          if (reservaton_channelWrapUpConfiguration && reservaton_channelWrapUpConfiguration.enabled){
+          if (existing_reservation.status === "wrapping") {
 
-            let date_last_updated = reservationExists.existing_reservation.dateUpdated;
-            let age_so_far_ms = (new Date() - new Date(date_last_updated));
-            let reservation_wrapUpTime_seconds = reservaton_channelWrapUpConfiguration.maxSeconds;
-            let remaining_time = (reservation_wrapUpTime_seconds*1000) - age_so_far_ms;
-            remaining_time = remaining_time <= 0 ? 1 : remaining_time;
+            if (reservation_channelWrapUpConfiguration && reservation_channelWrapUpConfiguration.enabled) {
+              let date_last_updated = existing_reservation.dateUpdated;
+              let age_so_far_ms = (new Date() - new Date(date_last_updated));
+              let reservation_wrapUpTime_seconds = reservation_channelWrapUpConfiguration.maxSeconds;
+              let remaining_time = (reservation_wrapUpTime_seconds * 1000) - age_so_far_ms;
+              remaining_time = remaining_time <= 0 ? 1 : remaining_time;
 
-            existingReservation_timer = setTimeout(()=> {
-              flex.Actions.invokeAction('CompleteTask', { sid: reservationExists.existing_reservation.sid });
-            }, remaining_time);
-
-            timers.set(reservationExists.existing_reservation.sid, existingReservation_timer);
+              existingReservation_timer = setTimeout(() => {
+                flex.Actions.invokeAction('CompleteTask', { sid: existing_reservation.sid });
+              }, remaining_time);
+              timers.set(existing_reservation.sid, existingReservation_timer);
+            }
           }
-              
-        }
-      
-        manager.workerClient.on('reservationCreated', reservation => {
-          // get the channel name for the reservation
 
-          let trueReservation = reservation.addListener?reservation:reservation.source;
+          else if (existing_reservation.status === "pending" || existing_reservation.status === "assigned" || existing_reservation.status === "accepted") {
+
+            let existing_reservation_completeTimer;
+            let trueExistingReservation = existing_reservation.addListener ? existing_reservation : existing_reservation.source;
+
+            trueExistingReservation.addListener('wrapup', payload => {
+
+              if (reservation_channelWrapUpConfiguration && reservation_channelWrapUpConfiguration.enabled) {
+                existing_reservation_completeTimer = setTimeout(() => {
+                  flex.Actions.invokeAction('CompleteTask', { sid: existing_reservation.sid });
+                }, reservation_channelWrapUpConfiguration.maxSeconds * 1000);
+                timers.set(existing_reservation.sid, existing_reservation_completeTimer);
+              }
+            });
+          }
+        });
+
+        manager.workerClient.on('reservationCreated', reservation => {
+        
+          let trueReservation = reservation.addListener ? reservation : reservation.source;
+
+          // get the channel name for the reservation
           let channelName = trueReservation.task.taskChannelUniqueName;
 
           //get the config time from the ui_attributes for the voice channel
@@ -89,10 +93,10 @@ export default class AutoCompleteWrapupPlugin extends FlexPlugin {
 
             //if the configuration exist and if the configuration for the channel is enabled
             //then wrapup the task within the time mentioned in the configuration for that channelName
-            if (channelWrapUpConfiguration && channelWrapUpConfiguration.enabled){
-              reservation_completeTimer = setTimeout(()=> {
+            if (channelWrapUpConfiguration && channelWrapUpConfiguration.enabled) {
+              reservation_completeTimer = setTimeout(() => {
                 flex.Actions.invokeAction('CompleteTask', { sid: reservation.sid });
-              }, channelWrapUpConfiguration.maxSeconds*1000);
+              }, channelWrapUpConfiguration.maxSeconds * 1000);
 
               timers.set(reservation.sid, reservation_completeTimer);
             }
@@ -105,24 +109,24 @@ export default class AutoCompleteWrapupPlugin extends FlexPlugin {
             timers.delete(payload.sid);
           }
         });
-        
-        // Check if there is any existing reservation that is in wrapping state when the agents LogsOut
-        // If there is an activity in wrapping state when the agent is logging out then complete it
-        flex.Actions.addListener("beforeLogout", () => {
-          let reservationExistsBeforeLogout = checkExistingReservation();
-          // console.log('Existing reservation',reservationExistsBeforeLogout)
-          if (reservationExistsBeforeLogout.wrapping === true){
-            flex.Actions.invokeAction('CompleteTask', { sid: reservationExistsBeforeLogout.existing_reservation.sid });
-          }
-        });
 
+        flex.Actions.addListener("beforeLogout", payload => {
+          console.log('Hi');
+          FlexState.workerTasks.forEach(reservation => {
+            if (reservation.status === "wrapping") {
+              console.log('Reservation is wrapping');
+              flex.Actions.invokeAction('CompleteTask', { sid: reservation.sid });
+            }
+          });
+        });
       }
 
       // else show a notification that the configuration does not exist
       else {
         flex.Notifications.showNotification('UnconfiguredAutoWrapupTimer');
       }
-     
-    }); 
+
+    });
+
   }
 }
--- src/helper/existingReservation.js
+++ src/helper/existingReservation.js
@@ -1,12 +1,51 @@
 import FlexState from '../states/FlexState';
 
+import { Manager } from "@twilio/flex-ui";
+
+export function getConfiguration() {
+    const wrapupConfig = Manager.serviceConfiguration?.ui_attributes?.autoWrapupTimer;
+    return wrapupConfig;
+}
+
 export function checkExistingReservation() {    
-    let wrapping_reservation = {'wrapping': false, 'existing_reservation': null};
-    FlexState.workerTasks.forEach(reservation => {
-      let existing_reservation = reservation;
-       if(existing_reservation.status === "wrapping"){    
-        wrapping_reservation = {'wrapping': true, 'existing_reservation': existing_reservation};
-       }       
-    });
-    return wrapping_reservation;
+
+    // let wrapping = false;
+    // let existing_reservations = []; 
+    
+    wrapupConfig = getConfiguration();
+
+    if (wrapupConfig){  
+
+        const timers = new Map();
+
+        FlexState.workerTasks.forEach(reservation => {
+
+            const existing_reservation = reservation;
+            let reservation_channelName = existing_reservation.taskChannelUniqueName;
+            let reservaton_channelWrapUpConfiguration = wrapupConfig[reservation_channelName];
+
+            // set a variable for timeout, this is used for clearing timeouts if the task is completed
+            let existingReservation_timer ;
+
+            if(existing_reservation.status === "wrapping"){
+
+                if (reservaton_channelWrapUpConfiguration && reservaton_channelWrapUpConfiguration.enabled){
+
+                    let date_last_updated = existing_reservation.dateUpdated;
+                    let age_so_far_ms = (new Date() - new Date(date_last_updated));
+                    let reservation_wrapUpTime_seconds = reservaton_channelWrapUpConfiguration.maxSeconds;
+                    let remaining_time = (reservation_wrapUpTime_seconds*1000) - age_so_far_ms;
+                    remaining_time = remaining_time <= 0 ? 1 : remaining_time;
+
+                    existingReservation_timer = setTimeout(()=> {
+                    flex.Actions.invokeAction('CompleteTask', { sid: existing_reservation.sid });
+                    }, remaining_time);
+
+                    timers.set(existing_reservation.sid, existingReservation_timer);
+                }
+           }
+        });
+    
+    }
+  
 }
